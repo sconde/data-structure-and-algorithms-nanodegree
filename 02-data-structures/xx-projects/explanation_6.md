856a599edc4d4beb8b586f6af267969f8c494a3d
Sidafa Conde <sconde@sandia.gov>
2020-09-01 17:45:46 -0400

    update problem 6

--- 02-data-structures/xx-projects/explanation_6.md
+++ 02-data-structures/xx-projects/explanation_6.md
@@ -1,13 +1,11 @@
 # Problem 6: Explanation
 
-For the __union and intersection__ problem, the approach has been to transform the _linked lists_, a format which is harder to work with, on something much __simpler__ as is a list. 
-Once the transformation has been done, the combination with the handy _object_ __sets__, has done all the work.
+For the union and intersection, I used the builtin `set` in python, leveraging that `set` only stores unique elements.
 
-## Time/Space complexity
+Time complexity:
+    - `append` and `size` are __O(n)__ and __O(m)__, where __n__ and __m__ are length of the two lists, respectively.
+    
+Space complexity:
+    - Storing the content of both list, should they be unique, leads to __O(n + m)__
+    - If the one list is a subset of the larger list, then with clever re-ordering, the storage could be reduced to __O(n)__, where __n > m__
 
-In the study of the __time complexity__, we find that the transformation from _linked list_ to list, takes __O(n)__  time complexity, the the set function is in the __same or less__ order of magnitude, as for the variations:
-
-- _Union_: we find the creation of the final array, again __O(n)__, making __n*O(n)__ be resulting to __O(n)__
-- _Intersection_: we find the creation of the final array, which is a _double for-loop_ (operation _x in s_, acts with  __O(n)__), resulting finally in __O(n^n)__
-
-In respect to the _space time complexity_, we generate for both functions, 3 auxiliary lists, being _O(3n)_; and resulting in __O(n)__.
--- 02-data-structures/xx-projects/problem_6.py
+++ 02-data-structures/xx-projects/problem_6.py
@@ -2,42 +2,55 @@
 Proble 6: Union and Intersection
 """
 
-class Node(object):
-    
+
+class Node:
+
     def __init__(self, val):
         self.value = val
         self.next = None
 
-class LinkedList(object):
-    
+    def __repr__(self):
+        return str(self.value)
+
+
+class LinkedList:
+
     def __init__(self):
         self.head = None
 
+    def __str__(self):
+        cur_head = self.head
+        out_string = ""
+        while cur_head:
+            out_string += str(cur_head.value) + " -> "
+            cur_head = cur_head.next
+
+        return out_string
+
     def append(self, val):
-        
+
         if self.head is None:
             self.head = Node(val)
             return
-        
-        head = self.head
-        while head.next:
-            head = head.next
 
-        head.next = Node(val)
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(val)
 
     def size(self):
-        
-        size = 0
-        head = self.head
 
-        while head:
-            size +=1
-            head = head.next
+        size = 0
+        node = self.head
+        while node:
+            size += 1
+            node = node.next
 
         return size
 
     def to_list(self):
-        
+
         this_list = []
         head = self.head
 
@@ -49,7 +62,6 @@ class LinkedList(object):
 
 
 def union(first_list, second_list):
-
     big_list = list(
         set(
             first_list.to_list() + second_list.to_list()
@@ -62,12 +74,12 @@ def union(first_list, second_list):
 
     return linked_list
 
+
 def intersection(first_list, second_list):
+    set_1 = set(first_list.to_list())
+    set_2 = set(second_list.to_list())
 
-    set_1 = set( first_list.to_list() )
-    set_2 = set( second_list.to_list())
-
-    common = [ el for el in set_1 if el in set_2 ]
+    common = [el for el in set_1 if el in set_2]
 
     linked_list = LinkedList()
     for el in common:
@@ -75,98 +87,37 @@ def intersection(first_list, second_list):
 
     return linked_list
 
-if __name__ == "__main__":
-    
-    #%% Test Official
-    # Normal Cases:
-    # Test case 1
-    linked_list_1 = LinkedList()
-    linked_list_2 = LinkedList()
 
-    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
-    element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
+# Test case 1
 
-    for i in element_1:
-        linked_list_1.append(i)
+linked_list_1 = LinkedList()
+linked_list_2 = LinkedList()
 
-    for i in element_2:
-        linked_list_2.append(i)
+element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
 
-    print(union(linked_list_1, linked_list_2))
-    # 32 -> 65 -> 2 -> 35 -> 3 -> 4 -> 6 -> 1 -> 9 -> 11 -> 21 ->
-    print(intersection(linked_list_1, linked_list_2))
-    # 4 -> 6 -> 21 ->
+for i in element_1:
+    linked_list_1.append(i)
 
-    # Test case 2
-    linked_list_3 = LinkedList()
-    linked_list_4 = LinkedList()
+for i in element_2:
+    linked_list_2.append(i)
 
-    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
-    element_2 = [1, 7, 8, 9, 11, 21, 1]
+print(union(linked_list_1, linked_list_2))
+print(intersection(linked_list_1, linked_list_2))
 
-    for i in element_1:
-        linked_list_3.append(i)
+# Test case 2
 
-    for i in element_2:
-        linked_list_4.append(i)
+linked_list_3 = LinkedList()
+linked_list_4 = LinkedList()
 
-    print(union(linked_list_3, linked_list_4))
-    # 65 -> 2 -> 35 -> 3 -> 4 -> 6 -> 1 -> 7 -> 8 -> 9 -> 11 -> 21 -> 23 ->
-    print(intersection(linked_list_3, linked_list_4))
-    #
+element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
+element_2 = [1, 7, 8, 9, 11, 21, 1]
 
-    # Test case 3
-    linked_list_5 = LinkedList()
-    linked_list_6 = LinkedList()
+for i in element_1:
+    linked_list_3.append(i)
 
-    element_1 = [22, 7, 4, 35, 6, 65, 6, 4, 3, 23]
-    element_2 = [1, 7, 8, 65, 11, 21, 1]
+for i in element_2:
+    linked_list_4.append(i)
 
-    for i in element_1:
-        linked_list_5.append(i)
-
-    for i in element_2:
-        linked_list_6.append(i)
-
-    print(union(linked_list_5, linked_list_6))
-    # 65 -> 1 -> 35 -> 4 -> 3 -> 6 -> 7 -> 8 -> 11 -> 21 -> 22 -> 23 ->
-    print(intersection(linked_list_5, linked_list_6))
-    # 65 -> 7 ->
-
-
-    # Edge Cases:
-    # Test case 4
-    linked_list_7 = LinkedList()
-    linked_list_8 = LinkedList()
-
-    element_1 = []
-    element_2 = [1, 7, 8]
-
-    for i in element_1:
-        linked_list_7.append(i)
-
-    for i in element_2:
-        linked_list_8.append(i)
-
-    print(union(linked_list_7, linked_list_8))
-    # 8 -> 1 -> 7 ->
-    print(intersection(linked_list_7, linked_list_8))
-    #
-
-    # Test case 5
-    linked_list_9 = LinkedList()
-    linked_list_10 = LinkedList()
-
-    element_1 = []
-    element_2 = []
-
-    for i in element_1:
-        linked_list_9.append(i)
-
-    for i in element_2:
-        linked_list_10.append(i)
-
-    print(union(linked_list_9, linked_list_10))
-    #
-    print(intersection(linked_list_9, linked_list_10))
-    #
+print(union(linked_list_3, linked_list_4))
+print(intersection(linked_list_3, linked_list_4))

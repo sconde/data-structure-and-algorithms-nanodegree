48996335ace9950f7288ef6fbd46a03f689ebb6e
Sidafa Conde <sconde@sandia.gov>
2020-09-03 13:14:42 -0400

    problem 1: update to reflect review

--- 02-data-structures/xx-projects/problem_1.py
+++ 02-data-structures/xx-projects/problem_1.py
@@ -11,8 +11,8 @@ class LRUCache:
         self.capacity = capacity
 
     def get(self, key):
-        val = self.cache.get(key, -1)
-        if val != -1:
+        val = self.cache.get(key)
+        if val:
             # O(1) time complexity ( python >= 3.2 )
             self.cache.move_to_end(key, last=False)
 
@@ -24,7 +24,9 @@ class LRUCache:
             return
 
         cache_hit = self.cache.get(key) is not None
+        print("cache hit = ", cache_hit)
         if cache_hit:
+            self.cache[key] = value
             self.cache.move_to_end(key, last=False)
         else:
             if len(self.cache) < self.capacity:
@@ -32,6 +34,7 @@ class LRUCache:
             else:
                 self.cache.popitem(last=False)
                 self.cache[key] = value
+
 
 
 # testing
@@ -45,8 +48,14 @@ our_cache.set(4, 4)
 print(our_cache.get(1))
 print(our_cache.get(2))
 print(our_cache.get(9))
-
 our_cache.set(5, 5)  
 our_cache.set(6, 6)
-
 print(our_cache.get(3))
+
+
+# review - case
+review_cache = LRUCache(5)
+review_cache.set(1, 1)
+print(review_cache.get(2) ) # returns -1
+review_cache.set(1, 11111)
+print(review_cache.get(1))  # overriding a value, returns 11111

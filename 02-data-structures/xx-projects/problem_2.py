6d87dc636ae0c3ceebb29839ee852720f6471dbd
Sidafa Conde <sconde@sandia.gov>
2020-09-03 13:06:44 -0400

    problem 2: review - hangle edge case

--- 02-data-structures/xx-projects/problem_2.py
+++ 02-data-structures/xx-projects/problem_2.py
@@ -20,13 +20,24 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    
+
+    # is the path just a file?
+    if os.path.isfile(path) and path.endswith(f'{suffix}'):
+        return [path]
+
+    if not os.path.isdir(path):
+        print(f"Invalid directory path {path}. Please supply a valid path.")
+        return []
+
     if suffix == "" or len(os.listdir(path)) == 0:
         return []
 
     path_content = os.listdir(path)
-    files = [file for file in path_content if file.endswith(f'.{suffix}')]
-    folders = [f for f in path_content if os.path.isdir(f'{path}/{f}')]
+    files = [file for file in path_content
+             if (file.endswith(f'{suffix}') and os.path.isfile(os.path.join(path, file)))
+             ]
+
+    folders = [f for f in path_content if os.path.isdir(os.path.join(path, f))]
 
     for folder in folders:
         files.extend(
@@ -42,15 +53,20 @@ def find_files(suffix, path):
 path_base = os.getcwd() + '/testdir'
 
 # Normal Cases:
-print(find_files(suffix='c', path=path_base))
+print(find_files(suffix='.c', path=path_base))
 # ['t1.c', 'a.c', 'a.c', 'b.c']
 
-print(find_files(suffix='h', path=path_base))
+print(find_files(suffix='.h', path=path_base))
 # ['t1.h', 'a.h', 'a.h', 'b.h']
 
-print(find_files(suffix='z', path=path_base))
+print(find_files(suffix='.z', path=path_base))
 # []
 
 # Edge Cases:
 print(find_files(suffix='', path=path_base))
 # []
+
+# another edge case ( from the review )
+path = "./problem_2.py"
+suffix = ".py"
+print(find_files(path=path, suffix=suffix))
